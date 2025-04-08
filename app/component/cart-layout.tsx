eca47d25b4c65942767c98e7d20859580713e031
nguyluky <nguyluky@gmail.com>
2025-04-08 21:47:40 +0700

    tạm nghỉ mai làm tiếp

--- app/component/cart-layout.tsx
+++ app/component/cart-layout.tsx
@@ -7,7 +7,7 @@ export default function CartLayout() {
       <header className="navbar justify-center">
         <div className="navbar-center w-full max-w-[80rem] pr-5">
           <div className="navbar-start">
-            <Link className="btn btn-ghost text-xl font-bold">Codelỏ</Link>
+            <Link to="/" className="btn btn-ghost text-xl font-bold">Codelỏ</Link>
             <div className="divider divider-horizontal"></div>
             <h2>Giỏ hàng</h2>
           </div>

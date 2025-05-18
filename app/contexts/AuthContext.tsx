e3c9e5793333db5b4daab1186aa8345f42ec7aca
nguyluky <151525707+nguyluky@users.noreply.github.com>
2025-05-18 13:57:42 +0700

    Refactor authentication context and improve user profile handling; update admin layout and sidebar for better access control and user experience

--- app/contexts/AuthContext.tsx
+++ app/contexts/AuthContext.tsx
@@ -1,73 +1,80 @@
-import { createContext, useContext, useEffect, useState } from "react";
-import { AuthService, type LoginRequest, type RegisterRequest } from "~/service/auth.service";
-import { ProfileService, type Profile } from "~/service/profile.service";
-
+import { createContext, useContext, useEffect, useState } from 'react';
+import type { Account } from '~/models/Account';
+import type { Profile } from '~/models/Profile';
+import { AuthService, type LoginRequest, type RegisterRequest } from '~/service/auth.service';
+import { ProfileService } from '~/service/profile.service';
 
 interface AuthContextState {
-    isAuthenticated: boolean,
-    user: Profile | null,
+    isAuthenticated: boolean;
+    profile: Profile | null;
+    account: Account | null;
     loading: boolean;
-    error: string | null,
+    error: string | null;
     login: (userData: LoginRequest) => Promise<void>;
     register: (userData: RegisterRequest) => Promise<void>;
-    logout: () => Promise<void>
+    logout: () => Promise<void>;
 }
-
 
 const defaultAuthContextState: AuthContextState = {
     isAuthenticated: false,
     loading: true,
     error: null,
-    user: null,
+    profile: null,
+    account: null,
     login: async () => {},
     register: async () => {},
-    logout: async () => {}
-} 
+    logout: async () => {},
+};
 
 export const AuthContext = createContext<AuthContextState>(defaultAuthContextState);
 
 // Auth provider props interface
 interface AuthProviderProps {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
 const authService = new AuthService();
 const profileService = new ProfileService();
 
-export const AuthProvider: React.FC<AuthProviderProps> = ({children}) => {
-
-    const [isAuthenticated,setIsAuthenticated ] = useState<boolean>(false);
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [user, setUser] = useState<Profile | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
-
+    const [account, setAccount] = useState<Account | null>(null);
 
     useEffect(() => {
-           const checkAuthentication = async () => {
-      const token = localStorage.getItem('token');
-      
-      if (!token) {
-        setLoading(false);
-        return;
-      }
-      
-      try {
-        const [userData , errr]= await profileService.getProfile();
-        if (errr) {
-            throw new Error(errr)
-        }
-        setUser(userData);
-        setIsAuthenticated(true);
-      } catch (err) {
-        console.error('Authentication error:', err);
-        localStorage.removeItem('token');
-      } finally {
-        setLoading(false);
-      }
-    };
-    
-    checkAuthentication();
-    }, [])
+        const checkAuthentication = async () => {
+            const token = localStorage.getItem('token');
+
+            if (!token) {
+                setLoading(false);
+                return;
+            }
+
+            try {
+                const [userData, errr] = await profileService.getProfile();
+                const [accountData, errorAccount] = await authService.getUserInfo();
+                if (errr) {
+                    throw new Error(errr);
+                }
+                if (errorAccount) {
+                    throw new Error(errorAccount);
+                }
+                console.log('userData', accountData);
+                setAccount(accountData?.user ?? null);
+                setUser(userData?.data ?? null);
+                setIsAuthenticated(true);
+            } catch (err) {
+                console.error('Authentication error:', err);
+                localStorage.removeItem('token');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        checkAuthentication();
+    }, []);
 
     const login = async (userData: LoginRequest) => {
         setLoading(true);
@@ -75,47 +82,58 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({children}) => {
         try {
             const [data, error] = await authService.login(userData);
             if (error) {
-              setError(error.message);
-              return;
+                setError(error.message);
+                return;
             }
             localStorage.setItem('token', data?.access_token || '');
             const [data1, error1] = await profileService.getProfile();
 
             if (error1) {
-                setError(String(error1))
+                setError(String(error1));
             }
 
-            setUser(data1);
+            setUser(data1?.data ?? null);
             setIsAuthenticated(true);
-                
+        } catch (err: any) {
+            alert(err);
+        } finally {
+            setLoading(false);
         }
-        catch (err: any) {
-            alert(err)
-        }
-        finally {
-            setLoading(false)
-        }
-
-
-    }
+    };
 
     const logout = async () => {
-        console.log('hello')
-
-    }
+        setLoading(true);
+        try {
+            // await authService.logout();
+            localStorage.removeItem('token');
+            setUser(null);
+            setAccount(null);
+            setIsAuthenticated(false);
+            setError(null);
+        } catch (err: any) {
+            setError(err.message || 'Logout failed');
+            console.error('Logout error:', err);
+        } finally {
+            setLoading(false);
+        }
+    };
 
     const register = async () => {
-        console.log('hello')
-    }
+        console.log('hello');
+    };
 
     const contextValue: AuthContextState = {
-        isAuthenticated, user, loading, error, login, logout, register
-    }
+        isAuthenticated,
+        profile: user,
+        loading,
+        error,
+        login,
+        logout,
+        register,
+        account,
+    };
 
-    return <AuthContext.Provider value={contextValue}>
-        {children}
-    </AuthContext.Provider>
-}
+    return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
+};
 
-
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => useContext(AuthContext);

15c86062e85e8a05e3bfdaa52f10704d32d89727
nguyluky <151525707+nguyluky@users.noreply.github.com>
2025-05-20 00:34:21 +0700

    xóa thanh search không cần thiết ở cart và thanh toán

--- app/component/cart-layout.tsx
+++ app/component/cart-layout.tsx
@@ -1,33 +1,56 @@
-import { Link, Outlet } from "react-router";
-import { Footer } from "./layout";
+import { Link, Outlet } from 'react-router';
+import { useAuth } from '~/contexts/AuthContext';
+import { Footer } from './layout';
 
 export default function CartLayout() {
-  return (
-    <>
-      <header className="navbar justify-center">
-        <div className="navbar-center w-full max-w-[80rem] pr-5">
-          <div className="navbar-start">
-            <Link to="/" className="btn btn-ghost text-xl font-bold">Codelỏ</Link>
-            <div className="divider divider-horizontal"></div>
-            <h2>Giỏ hàng</h2>
-          </div>
+    const { isAuthenticated, profile: user, account } = useAuth();
+    return (
+        <>
+            <header className="navbar justify-center">
+                <div className="navbar-center w-full max-w-[80rem] pr-5">
+                    <div className="navbar-start">
+                        <Link to="/" className="btn btn-ghost text-xl font-bold">
+                            Codelỏ
+                        </Link>
+                        <div className="divider divider-horizontal"></div>
+                        <h2>Giỏ hàng</h2>
+                    </div>
 
-          <div className="navbar-end">
-            <div className="join">
+                    <div className="navbar-end">
+                        {isAuthenticated ? (
+                            <>
+                                {account?.rule == 1 && (
+                                    <Link to="/admin/dashboard" className="btn btn-outline">
+                                        GOTO ADMIN
+                                    </Link>
+                                )}
+                                <Link
+                                    to="/tai-khoan"
+                                    className="btn btn-outline btn-primary hidden sm:flex"
+                                >
+                                    {user?.fullname || 'undefind'}
+                                </Link>
+                            </>
+                        ) : (
+                            <Link to="/auth/login" className="btn btn-primary hidden sm:flex">
+                                Đăng nhập
+                            </Link>
+                        )}
+                        {/* <div className="join">
               <div>
                 <div>
                   <input className="input join-item" placeholder="Search" />
                 </div>
               </div>
               <button className="btn join-item">Search</button>
-            </div>
-          </div>
-        </div>
-      </header>
-      <main className="flex-grow">
-        <Outlet />
-      </main>
-      <Footer></Footer>
-    </>
-  );
+            </div> */}
+                    </div>
+                </div>
+            </header>
+            <main className="flex-grow">
+                <Outlet />
+            </main>
+            <Footer></Footer>
+        </>
+    );
 }
